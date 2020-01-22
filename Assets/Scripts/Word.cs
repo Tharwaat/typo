2d7f7fcad94d88da0e474b08dca3829766c8106e
tharwaat <a.tharwat100@gmail.com>
2020-01-22 12:23:12 +0200

    Initial commit

---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word
+{
+    public string word;
+    private int typeIndex;
+    private WordDisplay display;
+    
+    public Word(string word, WordDisplay display){
+        this.word = word;
+        typeIndex = 0;
+        this.display = display;
+        this.display.SetWord(this.word);
+    }    
+
+    public char GetNextLetter(){
+        return this.word[typeIndex];
+    }
+
+    public void TypeLetter(){
+        this.typeIndex++;
+        display.RemoveLetter();
+    }
+
+    public bool WordTyped(){
+        bool finished = (typeIndex >= word.Length);
+        if (finished){
+            display.RemoveWord();
+        }
+        return finished;
+    }
+}
---
+++ Assets/Scripts/WordManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    public List<Word> words;
+    public WordSpawner spawner;
+    private bool hasActiveWord;
+    private Word ActiveWord;
+    
+    public void AddWord(){
+        Word word = new Word(WordGenerator.GetRandomWord(), spawner.SpawnWord());
+        Debug.Log(word.word);
+        words.Add(word);
+    }
+
+    public void TypeLetter(char letter){
+        if (hasActiveWord){
+            if (ActiveWord.GetNextLetter() == letter) ActiveWord.TypeLetter();
+        }
+        else{
+            foreach (Word word in words){
+                if (word.GetNextLetter() == letter){
+                    hasActiveWord = true;
+                    ActiveWord = word;
+                    word.TypeLetter();
+                    break;
+                }
+            }
+        } 
+
+        if (hasActiveWord && ActiveWord.WordTyped()){
+            hasActiveWord = false;
+            words.Remove(ActiveWord);
+        }
+    }
+}
